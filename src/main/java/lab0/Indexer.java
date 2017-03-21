0f99ae36a8cdcf123483866681da0e5420840ff9
paulo <pj.martins@campus.fct.unl.pt>
2017-03-21 18:42:06 +0000

    Update indexer to read from queries.txt

--- src/main/java/lab0/Indexer.java
+++ src/main/java/lab0/Indexer.java
@@ -8,16 +8,11 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -29,6 +24,7 @@ public class Indexer {
     private static String indexPath = "index";
     @SuppressWarnings("FieldCanBeLocal")
     private static String documentsPath = "Answers.csv";
+    private static String queriesPath = "src/main/java/lab0/evaluation/queries.txt";
 
     private IndexWriter openIndex(Analyzer analyzer) {
         IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
@@ -197,6 +193,81 @@ public class Indexer {
         }
     }
 
+    private void parseQueries(Analyzer analyzer) {
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("output.txt", "UTF-8");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(queriesPath));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(":", 2);
+                searchQuery(writer, analyzer, parts[0], parts[1]);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error parsing queries file");
+        }
+        writer.close();
+    }
+
+    private void searchQuery(PrintWriter writer, Analyzer analyzer, String queryID, String queryStr) {
+        IndexReader reader = null;
+
+        try {
+            reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
+        } catch (IOException e) {
+            System.out.println("Error opening index");
+        }
+
+        if (reader == null)
+            throw new NullPointerException("Reader is null");
+
+        IndexSearcher searcher = new IndexSearcher(reader);
+        QueryParser parser = new QueryParser("Body", analyzer);
+
+        try {
+            Query query = null;
+            try {
+                query = parser.parse(queryStr);
+            } catch (org.apache.lucene.queryparser.classic.ParseException e) {
+                System.out.println("Error parsing query string.");
+            }
+
+            TopDocs results = searcher.search(query, 10);
+            ScoreDoc[] hits = results.scoreDocs;
+
+
+            int numTotalHits = results.totalHits;
+            System.out.println(numTotalHits + " total matching documents");
+
+            int rank = 1;
+
+            for (ScoreDoc hit : hits) {
+
+                Document doc = searcher.doc(hit.doc);
+
+                writer.println(queryID + "\t" + "Q0" + "\t" + doc.getField("Id").numericValue().intValue()
+                        + "\t" + rank++ + "\t" + hit.score + "\t" + "run-1");
+
+//                String answer = doc.get("Body");
+//                Integer Id = doc.getField("Id").numericValue().intValue();
+//                System.out.println("DocId: " + Id);
+//                System.out.println("DocAnswer: " + answer);
+//                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main (String args[]) {
 
         Analyzer analyzer = new StandardAnalyzer();
@@ -215,8 +286,11 @@ public class Indexer {
             System.out.println("Error closing index");
         }
 
-        indexer.searchIndex(analyzer);
+        indexer.parseQueries(analyzer);
+
+        //indexer.searchIndex(analyzer);
     }
+
 
 
 }
